86050c460dbaaff33a351b0dd1fb7bd118611210
Lucio Ayala <you@example.com>
2022-08-04 19:36:22 -0300

    update

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -4,7 +4,7 @@ function BinarioADecimal(num) {
   // tu codigo aca
   let decimal = 0;
   for (let i=0;i<num.length;i++){
-    decimal += num[i] * 2 ** (num.length - 1 - i);
+    decimal += parseInt(num[i]) * 2 ** (num.length - 1 - i);
   }
   return decimal;
 }
