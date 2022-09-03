00255556c74e4d24f5124953e08b3aa73709f791
Lucio Ayala <you@example.com>
2022-09-03 09:21:38 -0300

    update

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,19 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+ if (array.length <= 1) return array;
+  //let pivote = array.length-1;
+  let pivote = array.length-1;
+  let left = [];
+  let rigth = [];
+  for (let i = 0; i < array.length;i++){
+    if (i !== pivote){
+      if(array[i] < array[pivote])
+        left.push(array[i]);
+        else rigth.push(array[i]);
+    }
+  }
+  return quickSort(left).concat(array[pivote]).concat(quickSort(rigth))
 }
 
 function mergeSort(array) {
@@ -14,7 +26,27 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <=1) return array;
+  let middle = Math.floor(array.length / 2);
+  let left = array.slice(0,middle);
+  let right = array.slice(middle);
+  return merge(mergeSort(left),mergeSort(right));
 
+}
+function merge(left,right){
+  let result = [];
+  let indexLeft = 0;
+  let indexRight = 0;
+  while(indexLeft<left.length && indexRight<right.length){
+    if(left[indexLeft]<right[indexRight]){
+      result.push(left[indexLeft]);
+      indexLeft++;
+    } else {
+      result.push(right[indexRight]);
+      indexRight++;
+    }
+  }
+  return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight));
 }
 
 // No modificar nada debajo de esta línea
